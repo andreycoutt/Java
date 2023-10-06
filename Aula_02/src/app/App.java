448b9d702e6c0c725b174c912b0590466093c722
andreycoutt <contato.andreyyy@gmail.com>
2023-10-05 23:44:10 -0300

    Aula 02 - Usando Visual Studio Code com Java

---
+++ Aula_02/src/app/App.java
@@ -0,0 +1,14 @@
+package app;
+
+import pacote1.Pessoa;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int i = 10;
+        i++;
+        System.out.println("Hello, World!");
+        System.out.println("the number is " + 10);
+        Pessoa p1 = new Pessoa();
+        p1.cumprimentar();
+    }
+}
---
+++ Aula_02/src/pacote1/Pessoa.java
@@ -0,0 +1,8 @@
+package pacote1;
+
+public class Pessoa {
+    
+    public void cumprimentar(){
+        System.out.println("hello JAVA!");
+    }
+}
