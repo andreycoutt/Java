4384750c4065eedf9e2f52295366f6437f9d86cf
andreycoutt <contato.andreyyy@gmail.com>
2023-11-24 22:56:32 -0300

    Aula 04 - Variáveis, Tipos de Dados e Operações Matemáticas

--- Aula_04/src/App.java
+++ Aula_04/src/App.java
@@ -3,6 +3,18 @@ public class App {
 
         int numero = 50;
 
-        System.out.println(numero);
+        int v1 = 50;
+        byte v2 = 30;
+        short v3 = 60;
+        long v4 = -8;
+
+        System.out.println("BYTE: " + Byte.MIN_VALUE + " a " + Byte.MAX_VALUE);
+        
+        System.out.println("Short: " + Short.MIN_VALUE + " a " + Short.MAX_VALUE);
+
+        System.out.println("Integer: " + Integer.MIN_VALUE + " a " + Integer.MAX_VALUE);
+        
+        System.out.println("Long: " + Long.MIN_VALUE + " a " + Long.MAX_VALUE);
+
     }
 }
